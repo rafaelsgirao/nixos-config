fe5c24445c980d0d11943e7017076cfbbf0ac680
Rafael Girão <git@rafael.ovh>
2023-11-28 17:42:17 +0000

    spy: Rewrite nvidia module for headless Nvidia card

--- modules/hardware/nvidia.nix
+++ modules/hardware/nvidia.nix
@@ -1,14 +1,30 @@
-{ config, ... }: {
-  # NVIDIA drivers are unfree.
-  nixpkgs.config.allowUnfree = true;
-  hardware.nvidia.powerManagement.enable = true;
-  hardware.nvidia.modesetting.enable = true;
+{ pkgs, ... }: {
 
-  hardware.nvidia.prime = {
-    #    offload.enable = true;
-    sync.enable = true;
-    nvidiaBusId = "PCI:01:00:0";
-    intelBusId = "PCI:00:02:0";
-  };
+  # disable intel.
+  # boot.blacklistedKernelModules = lib.mkIf (config.rg.class == "server") [ "i915" ];
+  # hardware.nvidia = {
+  #   prime = {
+  #     #    offload.enable = true;
+  #     sync.enable = true;
+  #     nvidiaBusId = "PCI:01:00:0";
+  #     intelBusId = "PCI:00:02:0";
+  #   };
+  # };
   services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement = {
+      enable = true;
+      finegrained = false; # requires PRIME to be set up
+    };
+    # open = true;
+    nvidiaSettings = false;
+  };
+
+  hardware.opengl = {
+    extraPackages = [
+      pkgs.nvidia-vaapi-driver
+    ];
+  };
 }
