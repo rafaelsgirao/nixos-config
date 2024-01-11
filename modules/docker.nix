c22f84493809e427b7ccfa118a7b4bad09906ee5
Rafael Girão <git@rafael.ovh>
2024-01-11 07:51:04 +0000

    Only freeze docker on workstations

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }: {
   virtualisation.docker = {
     enable = lib.mkDefault true;
     daemon.settings.shutdown-timeout = 120;
@@ -11,7 +11,7 @@
   #   setSocketvariable = true;
   # };
 
-  services.udev.extraRules = ''
+  services.udev.extraRules = lib.mkIf (config.rg.class == "workstation") ''
     SUBSYSTEM=="power_supply", ATTR{online}=="0", RUN+="${pkgs.systemd}/bin/systemctl freeze docker.service"
     SUBSYSTEM=="power_supply", ATTR{online}=="1", RUN+="${pkgs.systemd}/bin/systemctl thaw docker.service"
   '';
