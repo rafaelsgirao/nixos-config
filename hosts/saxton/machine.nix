6dc9fa8c94edc4bfdca1ef250c9780566e20af50
Rafael Girão <git@rafael.ovh>
2024-01-21 15:38:25 +0000

    Remove commented code

--- hosts/saxton/machine.nix
+++ hosts/saxton/machine.nix
@@ -41,16 +41,6 @@ in
 
   networking = {
     hostId = "f46e55a8";
-    # interfaces."eth0".ipv6 = {
-    #   addresses = [{
-    #     address = config.rg.ipv6;
-    #     prefixLength = 64;
-    #   }];
-    # };
-    # defaultGateway6 = {
-    #   address = "fe80::1";
-    #   interface = "eth0";
-    # };
   };
 
   networking.nameservers = [ config.rg.ip "1.1.1.1" ];
