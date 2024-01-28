15e89bad9081bf4a18c250668a533f147bd9f4f1
Rafael Girão <git@rafael.ovh>
2024-01-28 13:42:25 +0000

    Add calibre overlay patch for deACSM plugin to work

---
+++ overlays/calibre-acsm-patch.nix
@@ -0,0 +1,20 @@
+_:
+_final: prev:
+{
+  # https://github.com/Leseratte10/acsm-calibre-plugin/issues/68#issuecomment-1760654174
+  calibre = prev.calibre.overrideAttrs (attrs: {
+    preFixup = builtins.replaceStrings
+      [
+        ''
+          --prefix PYTHONPATH : $PYTHONPATH \
+        ''
+      ]
+      [
+        ''
+          --prefix LD_LIBRARY_PATH : ${prev.libressl.out}/lib \
+          --prefix PYTHONPATH : $PYTHONPATH \
+        ''
+      ]
+      attrs.preFixup;
+  });
+}
