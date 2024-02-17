3b958e326a2e8823b1f3e8f6f793e5d309c25799
Rafael Girão <git@rafael.ovh>
2024-02-17 09:17:54 +0000

    go-vod: Fix package

--- packages/go-vod/default.nix
+++ packages/go-vod/default.nix
@@ -3,16 +3,14 @@ buildGoModule rec {
   pname = "go-vod";
   version = "0.2.4";
 
-  src = fetchgit {
+  src = (fetchgit {
     url = "https://github.com/pulsejet/memories.git";
     sparseCheckout = [
       "go-vod"
     ];
     rev = "f5627da48868988615f17e5a5b66215cabf37323";
     sha256 = "sha256-ZP+8/Bdm3QBOJztizdm21m9vwB0BJYMbjPJvDr4/iC8=";
-  };
-
-  preBuild = "cd go-vod";
+  }) + "/go-vod";
 
   deleteVendor = true;
   # vendorHash = "sha256-KQr0DtyH3xzlFwsDl3MGLRRLQC4+EtdTOG7IhmNCzV4=";
