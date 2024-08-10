3d569d38a2f5477cc8d702b1150dc19f6a893a6b
Rafael Girão <git@rafael.ovh>
2024-08-10 13:23:22 +0100

    refactor(chromium): delete old attrs & add brave-specific policies

--- modules/workstation/chromium.nix
+++ modules/workstation/chromium.nix
@@ -2,7 +2,7 @@
 # Surprisingly, it also works on Flatpaked Brave!
 # https://github.com/flathub/com.brave.Browser/blob/master/brave.sh
 
-{ config, ... }: {
+{ ... }: {
 
   imports = [ ];
   programs.chromium = {
@@ -16,79 +16,31 @@
       "AllowDinosaurEasterEgg" = false;
       "BrowserAddPersonEnabled" = false;
       "BrowserGuestModeEnabled" = false;
-      "BuiltInDnsClientEnabled" =
-        false; # Disabling this in hopes of disabling internal chrome DNS caching. but no idea
-      # "ClearBrowsingDataOnExitList" = [  ]; - doesn't do anything to option in options
+      # Disabling this in hopes of disabling internal chromium DNS caching, fingers crossed:
+      "BuiltInDnsClientEnabled" = false;
       "3rdparty"."extensions" = {
-        "cjpalhdlnbpafiamejdnhcphjbkeiagm" = {
-          # uBlock Origin
-          #   #   # "toOverwrite"."userFilters" = "! 2022-10-21 https://meta.stackoverflow.com\n###hot-network-questions\n"; - replaced with custom list
-          #   #   #TODO: Check if userFilters is a valid thing
-          #   #   "toOverwrite"."filterLists" = [
-          #   #
-          #   #     #Privacy
-          #   #     "adguard-spyware"
-          #   #     "adguard-spyware-url" # Privacy->Adguard URL Tracking Protection
-          #   #     "block-lan" # Block Outsider Intrusion into LAN
-          #   #
-          #   #     #Ads
-          #   #     "adguard-generic"
-          #   #
-          #   #     #Annoyances
-          #   #     "adguard-annoyance"
-          #   #     "adguard-social"
-          #   #
-          #   #     #Malware domains
-          #   #     "curben-pup" # PUP Domains blocklist
-          #   #     "curben-phishing" # Malwra
-          #   #
-          #   #     "user-filters"
-          #   #     "ublock-filters"
-          #   #     "ublock-badware"
-          #   #     "ublock-privacy"
-          #   #     "ublock-quick-fixes"
-          #   #     "ublock-abuse"
-          #   #     "ublock-unbreak"
-          #   #     "easylist"
-          #   #     "easyprivacy"
-          #   #     "urlhaus-1"
-          #   #     "fanboy-cookiemonster" # - Great alternative to I don't care about cookies
-          #   #     "plowe-0"
-          #   #     "spa-1" # - Adguard Spanish/Portuguese filters
-          #   #     "https://raw.githubusercontent.com/quenhus/uBlock-Origin-dev-filter/main/dist/google_duckduckgo/all.txt"
-          #   #     "https://raw.githubusercontent.com/brunomiguel/antinonio/master/antinonio.txt"
-          #   #     "https://raw.githubusercontent.com/quenhus/uBlock-Origin-dev-filter/main/dist/all_search_engines/global.txt"
-          #   #     "https://rafael.ovh/rg_ublock.txt"
-          #   #   ];
-          #   # };
-          "nngceckbapebfimnlniiiahkandclblb" = {
-            #Bitwarden
-            "environment"."base" = "https://vault.rafael.ovh";
-          };
+        "nngceckbapebfimnlniiiahkandclblb" = {
+          #Bitwarden
+          "environment"."base" = "https://vault.rafael.ovh";
         };
       };
+      # Brave-specific policies.
+      "BraveAIChatEnabled" = false;
+      "BraveRewardsDisabled" = true;
+      "BraveVPNDisabled" = true;
+      "BraveWalletDisabled" = true;
+      "BraveShieldsEnabledForUrls" = [ ];
+    };
 
-      # Ungoogled chromium completely breaks auto extension update behaviour, even if you specify the link
-      # See the patches in their repo
-      # extensions = [
-      #   "cjpalhdlnbpafiamejdnhcphjbkeiagm;https://clients2.google.com/service/update2/crx" # uBlock Origin
-
-      # ];
-      # extensions = [
-      #   "fihnjjcciajhdojfnbdddfaoknhalnja" # I don't care about cookies
+    # Must-have extensions. I may add others manually & just use Brave Sync
+    extensions = [
       "ajhmfdgkijocedmfjonnpjfojldioehi" # Privacy Pass
-        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
-        #   "gbmgphmejlcoihgedabhgjdkcahacjlj" # Wallabagger
-        #   "ocgpenflpmgnfapjedencafcfakcekcd" # Redirector
-        "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
-        "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
-        # #      "oladmjdebphlnjjcnomfhhbfdldiimaf" # LibRedirect #TODO: not on web store :(
-        # ];
-        };
+      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+      "eimadpbcbfnmbkopoojfekhnkhdbieeh" # Dark Reader
+      "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
+    ];
+  };
 
-        # nixpkgs.config = { chromium = { enableWideVine = true; }; };
-
-        #Firejail!
-        # environment.systemPackages = with pkgs; [ ungoogled-chromium ];
-        }
+  # nixpkgs.config = { chromium = { enableWideVine = true; }; };
+}
 
