58e6ae3cb580a8802ec2de8573ae1071bcd19f4c
Rafael Girão <git@rafael.ovh>
2024-09-08 16:55:03 +0100

    chore(cluster): change node-b ip

--- hosts/node-b/machine.nix
+++ hosts/node-b/machine.nix
@@ -7,7 +7,7 @@
 
   rg = {
     machineId = "b0540a853280ce070a7279df66918088";
-    ip = "192.168.122.61";
+    ip = "192.168.122.41";
     ipv4 = "192.168.122.41";
     # ipv6 = "2a01:4f8:1c1e:aead::1";
     pubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH9gdgB5xSKVwTG4fAw4nIBV+HxY4pGOxbE/ciNyzMZW";
