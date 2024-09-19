b1763c79e472e4b97f62fb941a10e7ac0b909473
Rafael Girão <git@rafael.ovh>
2024-09-19 08:50:47 +0100

    fix(untested): start nextcloud-client correctly

--- modules/workstation/nextcloud-client.nix
+++ modules/workstation/nextcloud-client.nix
@@ -2,4 +2,12 @@ _: {
   hm.services.nextcloud-client.enable = true;
 
   environment.persistence."/state".users.rg.directories = [ ".config/Nextcloud" ];
+
+
+  # https://discourse.nixos.org/t/nextcloud-client-does-not-auto-start-in-gnome3/46492/7
+  systemd.user.services.nextcloud-client = {
+    Unit = {
+      After = pkgs.lib.mkForce "graphical-session.target";
+    };
+  };
 }
