93b0fe30b583e9e7afc8c92bede63df9ea656836
Rafael Girão <git@rafael.ovh>
2024-11-05 16:57:44 +0000

    fix: use correct wireshark package

--- modules/workstation/default.nix
+++ modules/workstation/default.nix
@@ -424,7 +424,10 @@ in
     allowedUDPPortRanges = allowedTCPPortRanges;
   };
 
-  programs.wireshark.enable = true;
+  programs.wireshark = {
+    enable = true;
+    package = pkgs.wireshark;
+  };
 
   environment.systemPackages = with pkgs; [
     mypkgs.agenix
