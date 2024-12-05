9b3883dcbc0a0a570f4611a22392eb4dd1c9998e
Rafael Girão <git@rafael.ovh>
2024-12-05 13:40:14 +0000

    chore: add sazed dec24 ssh key

--- flake.nix
+++ flake.nix
@@ -193,6 +193,7 @@
         "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIEwOBxayZyd/zGYyoTRN2rdIQM71nzVT3lISg2pNfrZRAAAABHNzaDo= Yubikey-U2F-Resident:"
         "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPmgxo7Qybw0V8WCXUxO3nmqL5j7ASX+9fX5Ilz9eoJ37nZbojfiOPSfuZ6FRBHlIkRQx4TSOS2GTOHJgG+UMa4= rg@vin3[TPM]"
         "ecdsa-sha2-nistp384 AAAAE2VjZHNhLXNoYTItbmlzdHAzODQAAAAIbmlzdHAzODQAAABhBOM7OW/R8j1PSKlXfSp3Q1boy2ZGr/MSLOGh4Rp/qzrjqplHarCGnzT+EVbTULnCcErOErSmMyyVPY38JIkMLP50EQkx0Yp2bCxZAr/46HQ48Wj016zyd3oHF9wC8NeEIA== rg@sazed3[TPM]"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG7foe85vNDLm0vyVVugR8ThC1VjHuAtqAQ/K2AAVE9r rg@sazed[dec '24]"
       ];
 
       secretsDir = self + "/secrets";
@@ -203,7 +204,7 @@
         listToAttrs (
           map (name: {
             inherit name;
-            value = inputs.nixpkgs.lib.nixosSystem rec {
+            value = inputs.nixpkgs.lib.nixosSystem {
               specialArgs = {
                 inherit sshKeys;
                 inherit inputs;
