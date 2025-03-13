bde852de81223187fe652ea66cad2a3ae75f5fec
Rafael Girão <git@rafael.ovh>
2025-03-13 23:45:42 +0000

    fix(hoid): actually force option

--- hosts/hoid/hardware.nix
+++ hosts/hoid/hardware.nix
@@ -1,4 +1,4 @@
-_:
+{ lib, ... }:
 let
   hddCfg = hddDev: {
     type = "disk";
@@ -35,7 +35,7 @@ in
 
   # Both HDDs compete to be the /boot partition: pick one
   #( picked the one that disko picked to mount on /mnt when running nixos-install)
-  fileSystems."/boot".device = "/dev/disk/by-partlabel/disk-hdd1-ESP";
+  fileSystems."/boot".device = lib.mkForce "/dev/disk/by-partlabel/disk-hdd1-ESP";
   rg.vCores = 16;
 
   boot.initrd.availableKernelModules = [
