9a34866ceb7bbca5f6cd831ad0d4c896008e7f67
Rafael Girão <git@rafael.ovh>
2025-04-23 21:32:44 +0100

    feat(flatpak): automatically add flathub repo

--- modules/workstation/flatpak.nix
+++ modules/workstation/flatpak.nix
@@ -10,4 +10,13 @@ _: {
     ];
 
   };
+
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+
 }
