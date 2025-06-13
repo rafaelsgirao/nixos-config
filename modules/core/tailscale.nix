7b8d58aa70c29e0d514f2f875dfa63209f0d195f
Rafael Girão <git@rafael.ovh>
2025-06-13 13:17:31 +0100

    feat: default enable tailscale routing features

--- modules/core/tailscale.nix
+++ modules/core/tailscale.nix
@@ -1,7 +1,9 @@
-_: {
+{ lib, ... }:
+{
   services.tailscale = {
     enable = true;
     openFirewall = true;
+    useRoutingFeatures = lib.mkDefault "client";
   };
   environment.persistence."/pst".directories = [ "/var/lib/tailscale" ];
   networking.firewall = {
