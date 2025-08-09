5d96cfa10b6c59fcdecfb5256b35cdd8a495a89f
Rafael Girão <git@rafael.ovh>
2025-08-09 13:39:56 +0100

    feat: use local disko flake

--- disko.sh
+++ disko.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 #sudo nix run --extra-experimental-features "nix-command flakes" 'github:nix-community/disko' -- --mode disko --flake .#vin # --disk main /dev/sdc
-sudo nix run --extra-experimental-features "nix-command flakes" 'github:nix-community/disko' -- --mode destroy,format,mount --flake .#adolin "$@" # --disk main /dev/nvme0n1
+sudo nix run --extra-experimental-features "nix-command flakes" '.#disko' -- --mode destroy,format,mount --flake .#adolin "$@" # --disk main /dev/nvme0n1
 #sudo nix run --extra-experimental-features 'github:nix-community/disko#disko-install' -- --flake .#vin --disk main /dev/sdc
 
 # or sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disk-config.nix
