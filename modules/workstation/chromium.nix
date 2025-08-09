4419f4d5d634ed12eb7cfb70320a782468d3fb52
Rafael Girão <git@rafael.ovh>
2025-08-09 12:45:28 +0100

    fix: fix brave?

--- modules/workstation/chromium.nix
+++ modules/workstation/chromium.nix
@@ -43,7 +43,6 @@
       "BraveRewardsDisabled" = true;
       "BraveVPNDisabled" = true;
       "BraveWalletDisabled" = true;
-      "BraveShieldsEnabledForUrls" = [ ];
       # "IPFSEnabled" = false;
 
     };
