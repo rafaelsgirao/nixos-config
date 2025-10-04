7e5ea7c8079b6d7a8fe67846dce63e15067122dc
Rafael Girão <git@rafael.ovh>
2025-10-04 15:19:54 +0100

    feat: default to bitwarden ssh

--- leras.nix
+++ leras.nix
@@ -11,6 +11,9 @@ _:
 
   home-manager.users.rg = {
     home.stateVersion = "25.05";
+    home.sessionVariables = {
+      SSH_AUTH_SOCK = "/Users/rg/.bitwarden-ssh-agent.sock";
+    };
   };
 
   system.stateVersion = 6;
